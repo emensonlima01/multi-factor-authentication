00e4298f1296c84b5b5b21090081a1fa27a0175a
Emenson Lima <emensonlima@outlook.com.br>
2025-05-05 17:14:12 -0300

    feat(model): atualizar OtpLog para suportar ObjectId e permitir Id nulo

--- src/backend/MFAApi/Models/OtpLog.cs
+++ src/backend/MFAApi/Models/OtpLog.cs
@@ -1,8 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace MFAApi.Models;
 
 public class OtpLog
 {
-    public string Id { get; set; } = string.Empty;
+    [BsonId] // Indica que esta propriedade é o _id
+    [BsonRepresentation(BsonType.ObjectId)] // Define o tipo como ObjectId no MongoDB
+    public string? Id { get; set; } // Alterado para string? e removido o valor padrão
+
+    [BsonRepresentation(BsonType.ObjectId)] // Garante que UserId seja armazenado como ObjectId se for o caso
     public string UserId { get; set; } = string.Empty;
     public string OtpCode { get; set; } = string.Empty;
     public DateTime ExpiresAt { get; set; }
